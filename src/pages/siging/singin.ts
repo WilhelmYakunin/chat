037baba8af844e3503381f810726412874072d2d
WilhelmYakunin <v.jakunin@inbox.ru>
2023-09-19 01:17:34 +0300

    fix: lint err

--- src/pages/siging/singin.ts
+++ src/pages/siging/singin.ts
@@ -2,7 +2,7 @@ import Header from '../../components/header/header';
 import Input from '../../components/input/input';
 import LabeledInput from '../../components/labeledInput/LabeledInput';
 import ErrMessage from '../../components/errMessage/ErrMessage';
-import { getChats, getUserInfo, login } from './actions';
+import { login } from './actions';
 import { words } from '../../langs/index';
 import { routes } from '../../router/routes';
 
@@ -13,7 +13,7 @@ import bem from 'bem-ts';
 import Block, { someObj } from '../../components/block/block';
 
 import router from '../../router/router';
-import store, { IChat } from '../../store/store';
+import store from '../../store/store';
 
 const block = bem('signin');
 export default class Login extends Block {
